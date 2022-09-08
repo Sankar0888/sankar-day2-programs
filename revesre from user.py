c4d0227d7ae0e57c41f10c9d39ec530ec391cbd3
Sankar0888 <112850154+Sankar0888@users.noreply.github.com>
2022-09-08 14:11:59 +0530

    Add files via upload

---
+++ revesre from user.py
@@ -0,0 +1,7 @@
+Number = int(input("Please Enter any Number: "))
+Reverse = 0
+while(Number > 0):
+    Remainder = Number %10
+    Reverse = (Reverse *10) + Remainder
+    Number = Number //10
+print("\n Reverse of entered number is = %d" %Reverse)
